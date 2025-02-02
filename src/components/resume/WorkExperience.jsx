0ba1371f2a8423b920d45bf7a4493d72a59d19f4
sliver <silverbusiness007@gmail.com>
2025-02-02 18:18:46 -0300

    Init Project

---
+++ src/components/resume/WorkExperience.jsx
@@ -0,0 +1,67 @@
+const WorkExperience = [
+    {
+        id: 1,
+        title: "Software Engineer",
+        company: "Netreo",
+        yearsActive: "May 2022 - Nov 2024",
+        information: [
+            "Developed and optimized core application features using React, Node.js, and Express, ensuring seamless frontend-backend integration.",
+            "Leveraged AWS cloud services and Kubernetes to ensure high availability and scalability of monitoring applications.",
+            "Designed and integrated RESTful APIs, enabling seamless data exchange between internal systems and third-party platforms such as BMC Helix ITSM"
+        ],
+    },
+    {
+        id: 2,
+        title: "Full Stack Developer",
+        company: "BearPlex",
+        yearsActive: "Apr 2019 - Mar 2022",
+        information: [
+            "Successfully delivered AI-driven applications that improved client operations and user engagement.",
+            "Integrated REST APIs for seamless communication between frontend and backend services, reducing data load times by 30%",
+            "Managed and optimized cloud-based deployment using AWS, ensuring reliable and secure hosting",
+        ],
+    },
+    {
+        id: 3,
+        title: "Software Developer",
+        company: "High6",
+        yearsActive: "Mar 2018 – Mar 2019",
+        information: [
+            "Planned, developed, tested, deployed, and maintained web applications and mobile applications",
+            "Worked well independently and on a team to solve problems."
+        ],
+    },
+    {
+        id: 4,
+        title: "Junior Software Developer",
+        company: "High6",
+        yearsActive: "Oct 2017 - Feb 2018",
+        information: [
+            "Served as a dedicated and supportive Intern in a fast-paced environment",
+            "Worked to solve complex problems using the latest in Mobile and Web Technologies.",
+        ],
+    },
+    // {
+    //     id: 5,
+    //     title: "IT Support Assistant",
+    //     company: "Varsity College",
+    //     yearsActive: "February 2015 - May 2015",
+    //     information: [
+    //         "Worked to support the senior support staff with day-to-day tasks.",
+    //         "Assisted with the evaluation and troubleshooting of hardware and software issues.",
+    //         "Maintaining computer Inventory & surplus equipment."
+    //     ],
+    // },
+    // {
+    //     id: 6,
+    //     title: "Student Ambassador",
+    //     company: "Varsity College",
+    //     yearsActive: "December 2014 - February 2015",
+    //     information: [
+    //         "Monitored registration for all prospective student",
+    //         "Assisted parents and prospective student with necessary registration details",
+    //     ],
+    // },
+];
+
+export default WorkExperience;
