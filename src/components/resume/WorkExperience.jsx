5ce98fa0708dd28c5bb57971b4eccf6a01961363
Sliva <alyssonsilva1706@gmail.com>
2025-03-08 21:49:34 -0300

    Change My Experience

--- src/components/resume/WorkExperience.jsx
+++ src/components/resume/WorkExperience.jsx
@@ -1,9 +1,9 @@
 const WorkExperience = [
     {
         id: 1,
-        title: "Software Engineer",
-        company: "Netreo",
-        yearsActive: "May 2022 - Nov 2024",
+        title: "Senior Full Stack Developer",
+        company: "NuWare",
+        yearsActive: "May 2022 - Present",
         information: [
             "Developed and optimized core application features using React, Node.js, and Express, ensuring seamless frontend-backend integration.",
             "Leveraged AWS cloud services and Kubernetes to ensure high availability and scalability of monitoring applications.",
@@ -13,8 +13,8 @@ const WorkExperience = [
     {
         id: 2,
         title: "Full Stack Developer",
-        company: "BearPlex",
-        yearsActive: "Apr 2019 - Mar 2022",
+        company: "Company X",
+        yearsActive: "Set 2019 - Apr 2022",
         information: [
             "Successfully delivered AI-driven applications that improved client operations and user engagement.",
             "Integrated REST APIs for seamless communication between frontend and backend services, reducing data load times by 30%",
@@ -23,45 +23,14 @@ const WorkExperience = [
     },
     {
         id: 3,
-        title: "Software Developer",
-        company: "High6",
-        yearsActive: "Mar 2018 – Mar 2019",
+        title: "Full Stack Developer",
+        company: "Syntactics",
+        yearsActive: "Nov 2017 – Aug 2019",
         information: [
             "Planned, developed, tested, deployed, and maintained web applications and mobile applications",
             "Worked well independently and on a team to solve problems."
         ],
-    },
-    {
-        id: 4,
-        title: "Junior Software Developer",
-        company: "High6",
-        yearsActive: "Oct 2017 - Feb 2018",
-        information: [
-            "Served as a dedicated and supportive Intern in a fast-paced environment",
-            "Worked to solve complex problems using the latest in Mobile and Web Technologies.",
-        ],
-    },
-    // {
-    //     id: 5,
-    //     title: "IT Support Assistant",
-    //     company: "Varsity College",
-    //     yearsActive: "February 2015 - May 2015",
-    //     information: [
-    //         "Worked to support the senior support staff with day-to-day tasks.",
-    //         "Assisted with the evaluation and troubleshooting of hardware and software issues.",
-    //         "Maintaining computer Inventory & surplus equipment."
-    //     ],
-    // },
-    // {
-    //     id: 6,
-    //     title: "Student Ambassador",
-    //     company: "Varsity College",
-    //     yearsActive: "December 2014 - February 2015",
-    //     information: [
-    //         "Monitored registration for all prospective student",
-    //         "Assisted parents and prospective student with necessary registration details",
-    //     ],
-    // },
+    }
 ];
 
 export default WorkExperience;
