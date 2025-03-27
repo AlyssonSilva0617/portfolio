453083a2f12b909bc5e2e87a103dc848c15f191d
Alysson Silva <alyssonsilva1706@gmail.com>
2025-03-26 23:06:24 -0300

    change action

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
         <section className="home container" id='home'>
             <div className="intro">
                 <img src={Me} alt="" className='home__img' width='250' />
-                <h1 className="home__name">Alysson Mederos</h1>
-                <span className="home__education">I'm a Senior FullStack Engineer</span>
+                <h1 className="home__name">Alysson Silva</h1>
+                <span className="home__education">I'm a Senior FullStackEngineer</span>
 
                 <HeaderSocials />
 
